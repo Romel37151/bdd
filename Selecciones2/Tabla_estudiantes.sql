68b8c6f69d99ca55a34fa01587cc5e62bffeff34
Romel37151 <dsan37151@gmail.com>
2024-03-11 19:41:17 -0500

    ejercicio de and y or reto 7

---
+++ Selecciones2/Tabla_estudiantes.sql
@@ -0,0 +1,46 @@
+-- tabla estudiantes
+drop table estudiantes
+
+create table estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	email varchar (50) not null,
+	fecha_nacimiento date not null,
+	constraint estidiantes_pk primary key(cedula)
+)
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1754247722','Pedro','Vasquez','d45@gmal.com','1/2/2003')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('5689412233','Jose','Andrango','rt45@gmal.com','18/12/2003')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('5896234477','Joaquin','Andrimba','ja85@gmal.com','23/8/2000')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1002857777','Juan','Pillajo','jq9@gmal.com','6/12/1996')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1556938526','Rosa','Alba','ra52@gmal.com','26/1/1999')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('9635852269','Ana','Quilo','aq23@gmal.com','23/9/2001')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('9632587425','Margarita','Achina','mpq3@gmal.com','3/4/2004')
+
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values ('8956247585','Jose','Cadena','cer56@gmal.com','5/11/2003')
+  
+select * from estudiantes
+
+select nombre, apellido from estudiantes
+where nombre like 'M%' or apellido like '%z'
+
+select nombre from estudiantes 
+where cedula like '18%' and cedula like '32'
+
+select nombre, apellido from estudiantes
+where cedula like '%06' or cedula like '17%'
---
+++ Selecciones2/Tabla_videojuegos.sql
@@ -0,0 +1,43 @@
+-- tabla videojuegos
+ drop table videojuegos
+  create table videojuegos(
+  	 codigo int not null,
+	 nombre varchar (100) not null,
+	  descripcion varchar (300),
+	  valoracion int not null,
+	  constraint videojuegos_pk primary key(codigo)
+  )
+  
+  insert into videojuegos(codigo,nombre,descripcion,valoracion)
+  values(895,'Juan','LOL','4')
+  
+  insert into videojuegos(codigo,nombre,descripcion,valoracion)
+  values(753,'Pedro','GTA','5')
+  
+  insert into videojuegos(codigo,nombre,descripcion,valoracion)
+  values(852,'Julian','FORNITE','4')
+  
+  insert into videojuegos(codigo,nombre,descripcion,valoracion)
+  values(147,'Jose','FREE FIRE','3')
+  
+  insert into videojuegos(codigo,nombre,descripcion,valoracion)
+  values(951,'Armando','VALORANT','5')
+  
+  
+  insert into videojuegos(codigo,nombre,valoracion)
+  values(963,'Homero','4')
+  insert into videojuegos(codigo,nombre,valoracion)
+  values(456,'Jorge','1')
+  insert into videojuegos(codigo,nombre,valoracion)
+  values(012,'Sara','3')
+  select * from videojuegos
+  
+  select * from videojuegos
+  where nombre like '%C%' or valoracion =4
+  
+  select * from videojuegos
+  where (codigo between 3 and 7) or valoracion =4 
+  
+  select * from videojuegos
+  where (descripcion='Guerra' and valoracion>3 and nombre like 'C%')or
+  (valoracion>4 and nombre like 'D%')
